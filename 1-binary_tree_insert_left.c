ddc50f5153549b3c1ab0d0336d94141a1c16f754
mostafaehab96 <mostafa96ehab@gmail.com>
2023-06-01 21:32:01 +0000

    Add free to the left previous node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,8 +19,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	parent->left = binary_tree_node(parent, value);
 	if (p)
+	{
 		parent->left->left = binary_tree_node(parent->left, p->n);
-
+		free(p);
+	}
 	return (parent->left);
 }
 
